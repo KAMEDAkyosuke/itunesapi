4375fabb6cfe1de5bbb46b596e01b0353f241bd6
KAMEDAkyosuke <organlounge@me.com>
2017-04-18 17:17:31 +0900

    remove stringer generated file

--- country/storefrontid.go
+++ country/storefrontid.go
@@ -2,152 +2,154 @@ package country
 
 import (
 	"errors"
-	"fmt"
 )
 
 var ErrConvertFail = errors.New("convert fail")
 
-// see: https://affiliate.itunes.apple.com/resources/documentation/linking-to-the-itunes-music-store/#appendix
 type StorefrontID uint32
 
-const (
-	DZ StorefrontID = 143563
-	AO StorefrontID = 143564
-	AI StorefrontID = 143538
-	AG StorefrontID = 143540
-	AR StorefrontID = 143505
-	AM StorefrontID = 143524
-	AU StorefrontID = 143460
-	AT StorefrontID = 143445
-	AZ StorefrontID = 143568
-	BH StorefrontID = 143559
-	BD StorefrontID = 143490
-	BB StorefrontID = 143541
-	BY StorefrontID = 143565
-	BE StorefrontID = 143446
-	BZ StorefrontID = 143555
-	BM StorefrontID = 143542
-	BO StorefrontID = 143556
-	BW StorefrontID = 143525
-	BR StorefrontID = 143503
-	VG StorefrontID = 143543
-	BN StorefrontID = 143560
-	BG StorefrontID = 143526
-	CA StorefrontID = 143455
-	KY StorefrontID = 143544
-	CL StorefrontID = 143483
-	CN StorefrontID = 143465
-	CO StorefrontID = 143501
-	CR StorefrontID = 143495
-	CI StorefrontID = 143527
-	HR StorefrontID = 143494
-	CY StorefrontID = 143557
-	CZ StorefrontID = 143489
-	DK StorefrontID = 143458
-	DM StorefrontID = 143545
-	DO StorefrontID = 143508
-	EC StorefrontID = 143509
-	EG StorefrontID = 143516
-	SV StorefrontID = 143506
-	EE StorefrontID = 143518
-	FI StorefrontID = 143447
-	FR StorefrontID = 143442
-	DE StorefrontID = 143443
-	GH StorefrontID = 143573
-	GR StorefrontID = 143448
-	GD StorefrontID = 143546
-	GT StorefrontID = 143504
-	GY StorefrontID = 143553
-	HN StorefrontID = 143510
-	HK StorefrontID = 143463
-	HU StorefrontID = 143482
-	IS StorefrontID = 143558
-	IN StorefrontID = 143467
-	ID StorefrontID = 143476
-	IE StorefrontID = 143449
-	IL StorefrontID = 143491
-	IT StorefrontID = 143450
-	JM StorefrontID = 143511
-	JP StorefrontID = 143462
-	JO StorefrontID = 143528
-	KZ StorefrontID = 143517
-	KE StorefrontID = 143529
-	KR StorefrontID = 143466
-	KW StorefrontID = 143493
-	LV StorefrontID = 143519
-	LB StorefrontID = 143497
-	LI StorefrontID = 143522
-	LT StorefrontID = 143520
-	LU StorefrontID = 143451
-	MO StorefrontID = 143515
-	MK StorefrontID = 143530
-	MG StorefrontID = 143531
-	MY StorefrontID = 143473
-	MV StorefrontID = 143488
-	ML StorefrontID = 143532
-	MT StorefrontID = 143521
-	MU StorefrontID = 143533
-	MX StorefrontID = 143468
-	MD StorefrontID = 143523
-	MS StorefrontID = 143547
-	NP StorefrontID = 143484
-	NL StorefrontID = 143452
-	NZ StorefrontID = 143461
-	NI StorefrontID = 143512
-	NE StorefrontID = 143534
-	NG StorefrontID = 143561
-	NO StorefrontID = 143457
-	OM StorefrontID = 143562
-	PK StorefrontID = 143477
-	PA StorefrontID = 143485
-	PY StorefrontID = 143513
-	PE StorefrontID = 143507
-	PH StorefrontID = 143474
-	PL StorefrontID = 143478
-	PT StorefrontID = 143453
-	QA StorefrontID = 143498
-	RO StorefrontID = 143487
-	RU StorefrontID = 143469
-	SA StorefrontID = 143479
-	SN StorefrontID = 143535
-	RS StorefrontID = 143500
-	SG StorefrontID = 143464
-	SK StorefrontID = 143496
-	SI StorefrontID = 143499
-	ZA StorefrontID = 143472
-	ES StorefrontID = 143454
-	LK StorefrontID = 143486
-	KN StorefrontID = 143548
-	LC StorefrontID = 143549
-	VC StorefrontID = 143550
-	SR StorefrontID = 143554
-	SE StorefrontID = 143456
-	CH StorefrontID = 143459
-	TW StorefrontID = 143470
-	TZ StorefrontID = 143572
-	TH StorefrontID = 143475
-	BS StorefrontID = 143539
-	TT StorefrontID = 143551
-	TN StorefrontID = 143536
-	TR StorefrontID = 143480
-	TC StorefrontID = 143552
-	UG StorefrontID = 143537
-	GB StorefrontID = 143444
-	UA StorefrontID = 143492
-	AE StorefrontID = 143481
-	UY StorefrontID = 143514
-	US StorefrontID = 143441
-	UZ StorefrontID = 143566
-	VE StorefrontID = 143502
-	VN StorefrontID = 143471
-	YE StorefrontID = 143571
-)
+func (i StorefrontID) String() string {
+	return enum[uint32(i)]
+}
 
 func Convert(i uint32) (StorefrontID, error) {
-	code := StorefrontID(i).String()
-	s := fmt.Sprintf("StorefrontID(%d)", i)
-	if code == s {
+	_, ok := enum[i]
+	if !ok {
 		return 0, ErrConvertFail
 	}
 	return StorefrontID(i), nil
+}
+
+// see: https://affiliate.itunes.apple.com/resources/documentation/linking-to-the-itunes-music-store/#appendix
+var enum = map[uint32]string{
+	143563: "DZ",
+	143564: "AO",
+	143538: "AI",
+	143540: "AG",
+	143505: "AR",
+	143524: "AM",
+	143460: "AU",
+	143445: "AT",
+	143568: "AZ",
+	143559: "BH",
+	143490: "BD",
+	143541: "BB",
+	143565: "BY",
+	143446: "BE",
+	143555: "BZ",
+	143542: "BM",
+	143556: "BO",
+	143525: "BW",
+	143503: "BR",
+	143543: "VG",
+	143560: "BN",
+	143526: "BG",
+	143455: "CA",
+	143544: "KY",
+	143483: "CL",
+	143465: "CN",
+	143501: "CO",
+	143495: "CR",
+	143527: "CI",
+	143494: "HR",
+	143557: "CY",
+	143489: "CZ",
+	143458: "DK",
+	143545: "DM",
+	143508: "DO",
+	143509: "EC",
+	143516: "EG",
+	143506: "SV",
+	143518: "EE",
+	143447: "FI",
+	143442: "FR",
+	143443: "DE",
+	143573: "GH",
+	143448: "GR",
+	143546: "GD",
+	143504: "GT",
+	143553: "GY",
+	143510: "HN",
+	143463: "HK",
+	143482: "HU",
+	143558: "IS",
+	143467: "IN",
+	143476: "ID",
+	143449: "IE",
+	143491: "IL",
+	143450: "IT",
+	143511: "JM",
+	143462: "JP",
+	143528: "JO",
+	143517: "KZ",
+	143529: "KE",
+	143466: "KR",
+	143493: "KW",
+	143519: "LV",
+	143497: "LB",
+	143522: "LI",
+	143520: "LT",
+	143451: "LU",
+	143515: "MO",
+	143530: "MK",
+	143531: "MG",
+	143473: "MY",
+	143488: "MV",
+	143532: "ML",
+	143521: "MT",
+	143533: "MU",
+	143468: "MX",
+	143523: "MD",
+	143547: "MS",
+	143484: "NP",
+	143452: "NL",
+	143461: "NZ",
+	143512: "NI",
+	143534: "NE",
+	143561: "NG",
+	143457: "NO",
+	143562: "OM",
+	143477: "PK",
+	143485: "PA",
+	143513: "PY",
+	143507: "PE",
+	143474: "PH",
+	143478: "PL",
+	143453: "PT",
+	143498: "QA",
+	143487: "RO",
+	143469: "RU",
+	143479: "SA",
+	143535: "SN",
+	143500: "RS",
+	143464: "SG",
+	143496: "SK",
+	143499: "SI",
+	143472: "ZA",
+	143454: "ES",
+	143486: "LK",
+	143548: "KN",
+	143549: "LC",
+	143550: "VC",
+	143554: "SR",
+	143456: "SE",
+	143459: "CH",
+	143470: "TW",
+	143572: "TZ",
+	143475: "TH",
+	143539: "BS",
+	143551: "TT",
+	143536: "TN",
+	143480: "TR",
+	143552: "TC",
+	143537: "UG",
+	143444: "GB",
+	143492: "UA",
+	143481: "AE",
+	143514: "UY",
+	143441: "US",
+	143566: "UZ",
+	143502: "VE",
+	143471: "VN",
+	143571: "YE",
 }
